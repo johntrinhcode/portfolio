789a1d933d4521804ba77cb647f106f58df22e33
John <johntrinhcode@gmail.com>
2019-07-28 21:49:46 -0400

    project start up

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+        <h1 class="font-bold text-2xl text-blue-300">Example Project</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
