a39b9d635b34fc54db76548731a63dbe83efb9c7
John <johntrinhcode@gmail.com>
2019-07-29 00:33:46 -0400

    started navbar

---
+++ src/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="home" class="bg-gray-400 h-full w-32 font-sans text-xl text-white font-medium" >
+        home
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="nav" class="bg-gray-800 h-32 w-full font-sans text-xl text-white font-medium flex" >
+        <Home/>
+        <Links/>
+    </div>
+</template>
+
+<script>
+import Home from './Home.vue';
+import Links from './Links.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Home,
+    Links,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
