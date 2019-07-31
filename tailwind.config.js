4d3172af80292c44133196be1cccb4ef91932c09
johntrinhcode <johntrinhcode@gmail.com>
2019-07-31 17:45:02 -0400

    upgraded to tailwind 1.0+

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
-// tailwind.config.js
-module.exports = {
+
+  module.exports = {
+    prefix: '',
+    important: false,
+    separator: ':',
     theme: {
       fontFamily: {
           'body': ['object-sans', 'sans-serif'],
-      }
-    }
+      },
+    },
+    variants: {
+      appearance: ['responsive'],
+      // ...
+      zIndex: ['responsive'],
+    },
+    plugins: [
+      // ...
+    ],
   }
