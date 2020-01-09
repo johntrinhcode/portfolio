34a06e6386b9ce77b2ed967897b208b1cf18015b
John Trinh <johntrinhcode@gmail.com>
2020-01-09 17:23:46 -0500

    fixed route name

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,11 @@
 import Landing from "./components/page/Landing.vue";
+import App from "./App.vue";
 import Code from "./components/page/Code.vue";
 import Photo from "./components/page/Photo.vue";
 import Contact from "./components/page/Contact.vue";
 
 export default [
-  { path: "/", redirect: "/code" },
+  { path: "/", name: "App", component: App, props: true },
   { path: "/code", name: "Code", component: Code, props: true },
   { path: "/photo", name: "Photo", component: Photo, props: true },
   { path: "/contact", name: "Contact", component: Contact, props: true }
