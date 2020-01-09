c2f3264fba26a44261be15d58187140fbf08e7ab
johntrinhcode@gmail.com <johntrinhcode@gmail.com>
2020-01-08 20:33:55 -0500

    photo responseiveness

--- src/components/page/Photo.vue
+++ src/components/page/Photo.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="w-full h-full">
     <transition name="fade" mode="out-in" id="photo" class="w-full h-full">
-      <div v-if="currentItem == 0" :key="0" class="flex w-full h-full">
-        <v-lazy-image :src="rows[currentItem][0]" class="w-1/3 object-cover"></v-lazy-image>
-        <v-lazy-image :src="rows[currentItem][1]" class="w-1/3 object-cover"></v-lazy-image>
-        <v-lazy-image :src="rows[currentItem][2]" class="w-1/3 object-cover"></v-lazy-image>
+      <div v-if="currentItem == 0" :key="0" class="md:flex md:flex-row w-full h-full">
+        <v-lazy-image :src="rows[currentItem][0]" class="h-screen md:w-1/3 object-cover"></v-lazy-image>
+        <v-lazy-image :src="rows[currentItem][1]" class="h-screen md:w-1/3 object-cover"></v-lazy-image>
+        <v-lazy-image :src="rows[currentItem][2]" class="h-screen md:w-1/3 object-cover"></v-lazy-image>
       </div>
-      <div v-else-if="currentItem == 1" :key="1" class="flex w-full h-full">
-        <v-lazy-image :src="rows[currentItem][0]" class="w-1/3 object-cover"></v-lazy-image>
-        <v-lazy-image :src="rows[currentItem][1]" class="w-1/3 object-cover"></v-lazy-image>
-        <v-lazy-image :src="rows[currentItem][2]" class="w-1/3 object-cover"></v-lazy-image>
+      <div v-else-if="currentItem == 1" :key="1" class="md:flex md:flex-row w-full h-full">
+        <v-lazy-image :src="rows[currentItem][0]" class="h-screen md:w-1/3 object-cover"></v-lazy-image>
+        <v-lazy-image :src="rows[currentItem][1]" class="h-screen md:w-1/3 object-cover"></v-lazy-image>
+        <v-lazy-image :src="rows[currentItem][2]" class="h-screen md:w-1/3 object-cover"></v-lazy-image>
       </div>
-      <div v-else :key="2" class="flex w-full h-full">
-        <v-lazy-image :src="rows[currentItem][0]" class="w-1/3 object-cover"></v-lazy-image>
-        <v-lazy-image :src="rows[currentItem][1]" class="w-1/3 object-cover"></v-lazy-image>
-        <v-lazy-image :src="rows[currentItem][2]" class="w-1/3 object-cover"></v-lazy-image>
+      <div v-else :key="2" class="md:flex md:flex-row w-full h-full">
+        <v-lazy-image :src="rows[currentItem][0]" class="h-screen md:w-1/3 object-cover"></v-lazy-image>
+        <v-lazy-image :src="rows[currentItem][1]" class="h-screen md:w-1/3 object-cover"></v-lazy-image>
+        <v-lazy-image :src="rows[currentItem][2]" class="h-screen md:w-1/3 object-cover"></v-lazy-image>
       </div>
     </transition>
   </div>
