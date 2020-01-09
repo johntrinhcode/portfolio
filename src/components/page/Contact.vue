7d239af3827bb2c76ed885339226ac81075e9730
John Trinh <johntrinhcode@gmail.com>
2020-01-08 19:55:18 -0500

    fixed contact page

--- src/components/page/Contact.vue
+++ src/components/page/Contact.vue
@@ -1,15 +1,11 @@
 <template>
-  <div id="contact" class="relative flex w-full h-screen text-white">
-    <div id="contact-info" class="flex flex-col my-auto mx-auto text-center">
-      <p class="email font-head font-bold text-5xl">Thanks for looking,</p>
-      <p>you can find me at:</p>
+  <div id="contact" class="relative flex flex-col w-full h-full text-white">
+    <div id="contact-info" class="flex flex-col w-full items-center my-auto mx-auto text-center">
+      <p class="font-head font-bold text-xl leading-none">Thanks for looking,</p>
+      <p class="text-xs">you can reach me at:</p>
       <div class="relative flex w-full">
         <div class="flex items-center mx-auto mt-4">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            class="w-6 h-6 fill-current text-white"
-          >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-6 h-6 fill-current text-white">
             <path
               class="heroicon-ui"
               d="M4 4h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6c0-1.1.9-2 2-2zm16 3.38V6H4v1.38l8 4 8-4zm0 2.24l-7.55 3.77a1 1 0 0 1-.9 0L4 9.62V18h16V9.62z"
@@ -28,19 +24,4 @@ export default {
 };
 </script>
 
-<style>
-.email {
-  line-height: 0.75;
-}
-
-#contact-info {
-  position: absolute;
-  right: 0;
-  left: 0;
-  bottom: 0;
-  top: 0;
-
-  height: 16rem;
-  margin: auto;
-}
-</style>
+<style></style>
