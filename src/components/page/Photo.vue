6a37026bd246fd9648eea3fbd959abdd19ca9737
John Trinh <johntrinhcode@gmail.com>
2020-01-14 18:13:33 -0500

    photos now correctly display on mobile and scroll horizontally

--- src/components/page/Photo.vue
+++ src/components/page/Photo.vue
@@ -1,20 +1,28 @@
 <template>
   <div class="w-full h-full bg-black">
     <transition name="fade" mode="out-in" id="photo" class="w-full h-full">
-      <div v-if="currentItem == 0" :key="0" class="md:flex md:flex-row w-full h-full">
-        <v-lazy-image :src="rows[currentItem][0]" class="h-screen md:w-1/3 object-cover"></v-lazy-image>
-        <v-lazy-image :src="rows[currentItem][1]" class="h-screen md:w-1/3 object-cover"></v-lazy-image>
-        <v-lazy-image :src="rows[currentItem][2]" class="h-screen md:w-1/3 object-cover"></v-lazy-image>
+      <div
+        v-if="currentItem == 0"
+        :key="0"
+        class="flex md:flex-row w-screen h-full overflow-scroll"
+      >
+        <v-lazy-image :src="rows[currentItem][0]" class="md:w-1/3 object-cover"></v-lazy-image>
+        <v-lazy-image :src="rows[currentItem][1]" class="md:w-1/3 object-cover"></v-lazy-image>
+        <v-lazy-image :src="rows[currentItem][2]" class="md:w-1/3 object-cover"></v-lazy-image>
       </div>
-      <div v-else-if="currentItem == 1" :key="1" class="md:flex md:flex-row w-full h-full">
-        <v-lazy-image :src="rows[currentItem][0]" class="h-screen md:w-1/3 object-cover"></v-lazy-image>
-        <v-lazy-image :src="rows[currentItem][1]" class="h-screen md:w-1/3 object-cover"></v-lazy-image>
-        <v-lazy-image :src="rows[currentItem][2]" class="h-screen md:w-1/3 object-cover"></v-lazy-image>
+      <div
+        v-else-if="currentItem == 1"
+        :key="1"
+        class="flex md:flex-row w-screen h-full overflow-scroll"
+      >
+        <v-lazy-image :src="rows[currentItem][0]" class="md:w-1/3 object-cover"></v-lazy-image>
+        <v-lazy-image :src="rows[currentItem][1]" class="md:w-1/3 object-cover"></v-lazy-image>
+        <v-lazy-image :src="rows[currentItem][2]" class="md:w-1/3 object-cover"></v-lazy-image>
       </div>
-      <div v-else :key="2" class="md:flex md:flex-row w-full h-full">
-        <v-lazy-image :src="rows[currentItem][0]" class="h-screen md:w-1/3 object-cover"></v-lazy-image>
-        <v-lazy-image :src="rows[currentItem][1]" class="h-screen md:w-1/3 object-cover"></v-lazy-image>
-        <v-lazy-image :src="rows[currentItem][2]" class="h-screen md:w-1/3 object-cover"></v-lazy-image>
+      <div v-else :key="2" class="flex md:flex-row w-screen h-full overflow-scroll">
+        <v-lazy-image :src="rows[currentItem][0]" class="md:w-1/3 object-cover"></v-lazy-image>
+        <v-lazy-image :src="rows[currentItem][1]" class="md:w-1/3 object-cover"></v-lazy-image>
+        <v-lazy-image :src="rows[currentItem][2]" class="md:w-1/3 object-cover"></v-lazy-image>
       </div>
     </transition>
   </div>
@@ -26,9 +34,21 @@ export default {
     return {
       currentItem: 0,
       rows: [
-        [require("@/assets/photography/r1-1.jpg"), require("@/assets/photography/r1-2.jpg"), require("@/assets/photography/r1-3.jpg")],
-        [require("@/assets/photography/r2-1.jpg"), require("@/assets/photography/r2-2.jpg"), require("@/assets/photography/r2-3.jpg")],
-        [require("@/assets/photography/r3-1.jpg"), require("@/assets/photography/r3-2.jpg"), require("@/assets/photography/r3-3.jpg")]
+        [
+          require("@/assets/photography/r1-1.jpg"),
+          require("@/assets/photography/r1-2.jpg"),
+          require("@/assets/photography/r1-3.jpg")
+        ],
+        [
+          require("@/assets/photography/r2-1.jpg"),
+          require("@/assets/photography/r2-2.jpg"),
+          require("@/assets/photography/r2-3.jpg")
+        ],
+        [
+          require("@/assets/photography/r3-1.jpg"),
+          require("@/assets/photography/r3-2.jpg"),
+          require("@/assets/photography/r3-3.jpg")
+        ]
       ]
     };
   },
