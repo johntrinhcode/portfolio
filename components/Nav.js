46c8dddda79f2e7537089c47ca2aae1feed16514
John Trinh <john@johns-air.myfiosgateway.com>
2021-07-11 13:15:16 -0400

    fixed nav styling and beginning set up for blog posts using markdown files

--- components/Nav.js
+++ components/Nav.js
@@ -2,26 +2,41 @@ import React from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 
-export default function Nav() {
+export default function Nav(props) {
     const router = useRouter();
-    const { pid } = router.query;
+    const homeUnderline = (props.isHomeVisible) ? <div className="absolute bottom-0 -mb-2 rounded-md h-1 w-full bg-blue-300 transition-all duration-200 opacity-100"></div> : <div className="absolute bottom-0 -mb-2 rounded-md h-1 w-full transition-all duration-200 opacity-0"></div>
+    const blogUnderline = (props.isBlogVisible) ? <div className="absolute bottom-0 -mb-2 rounded-md h-1 w-full bg-blue-300 transition-all duration-200 opacity-100"></div> : <div className="absolute bottom-0 -mb-2 rounded-md h-1 w-full transition-all duration-200 opacity-0"></div>
+    const projectsUnderline = (props.isProjectsVisible) ? <div className="absolute bottom-0 -mb-2 rounded-md h-1 w-full bg-blue-300 transition-all duration-200 opacity-100"></div> : <div className="absolute bottom-0 -mb-2 rounded-md h-1 w-full transition-all duration-200 opacity-0"></div>
+    const contactUnderline = (props.isContactVisible) ? <div className="absolute bottom-0 -mb-2 rounded-md h-1 w-full bg-blue-300 transition-all duration-200 opacity-100"></div> : <div className="absolute bottom-0 -mb-2 rounded-md h-1 w-full transition-all duration-200 opacity-0"></div>
 
     return (
         <nav className="fixed top-0 z-20 w-full flex px-8 self-end justify-end flex-row space-x-8 h-16 items-center text-gray-400 bg-white">
             <Link href="/">
-                <span className="cursor-pointer hover:text-blue-300">Home</span>
+                <div className="relative flex flex-col">
+                    <span className="cursor-pointer hover:text-blue-300">Home</span>
+                    { homeUnderline }
+                </div>
             </Link>
 
             <Link href="#blog">
-                <span className="cursor-pointer hover:text-blue-300">Blog</span>
+                <div className="relative flex flex-col">
+                    <span className="cursor-pointer hover:text-blue-300">Blog</span>
+                    { blogUnderline }
+                </div>
             </Link>
 
             <Link href="#projects">
-                <span className="cursor-pointer hover:text-blue-300">Projects</span>
+                <div className="relative flex flex-col">
+                    <span className="cursor-pointer hover:text-blue-300">Projects</span>
+                    { projectsUnderline }
+                </div>
             </Link>
 
             <Link href="#contact">
-                <span className="cursor-pointer hover:text-blue-300">Contact</span>
+                <div className="relative flex flex-col">
+                    <span className="cursor-pointer hover:text-blue-300">Contact</span>
+                    { contactUnderline }
+                </div>
             </Link>
         </nav>
     );
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,23 @@
 import Head from 'next/head'
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import Title from '../components/Title'
 import Intro from '../components/Intro'
 import Topics from '../components/Topics'
 import Footer from '../components/Footer'
 import Nav from '../components/Nav'
+import useOnScreen from '../components/utilities/useOnScreenHook';
+import { InView } from 'react-intersection-observer';
 
 export default function Home() {
+  let [isHomeVisible, setIsHomeVisible] = useState(false);
+  let [isBlogVisible, setIsBlogVisible] = useState(false);
+  let [isProjectsVisible, setIsProjectsVisible] = useState(false);
+  let [isContactVisible, setIsContactVisible] = useState(false);
+  // const { homeRef, isHomeVisible, homeEntry } = useInView({ threshold: 0, trackVisibility: true, initialInView: true })
+  // const { blogRef, isBlogVisible, blogEntry } = useInView()
+  // const { projectsRef, isProjectsVisible, projectsEntry } = useInView()
+  // const { contactRef, isContactVisible, contactEntry } = useInView()
+  
   return (
     <div className="w-full h-full">
       <Head>
@@ -14,29 +25,60 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="w-screen flex flex-col items-center justify-center">
-        <Nav />
-        <div className="relative w-full h-screen flex flex-col items-center justify-center">
-          <iframe className="z-0 absolute" src='https://my.spline.design/untitled-2a1f4898dfe50ac9f3841139d82eecce/' frameborder='0' width='100%' height='100%'></iframe>
-          <span className="z-10 tracking-tighter font-display font-bold text-6xl text-gray-800">John Trinh</span>
-          <div className="z-10 flex flex-row space-x-4 font-display text-gray-400">
-            <span>Software Developer</span>
-            <span>Designer</span>
-            <span>Communicator</span>
+        <Nav isHomeVisible={isHomeVisible} isBlogVisible={isBlogVisible} isProjectsVisible={isProjectsVisible} isContactVisible={isContactVisible} />
+        <InView className="w-full" threshold={0.5} onChange={(inView, entry) => setIsHomeVisible(inView)}>
+            <div className="relative w-full h-screen flex flex-col md:flex-row items-center justify-center px-10 md:px-36">
+            {/* On small screens, image comes before hero text. */}
+            <div className="w-full md:w-0 h-screen md:h-0 flex flex-col items-center justify-end">
+              <div className="w-48 h-48 md:invisible">
+                <img src="image_1.png"></img>
+              </div>
+            </div>
+
+            <div className="w-full h-screen flex flex-col items-center justify-start md:justify-center mt-10 md:mt-0">
+              <div className="w-full">
+                <h1 className="z-10 tracking-tighter font-display font-bold text-6xl text-center md:text-left">
+                  <span className="text-gray-400">Hey, i'm </span>
+                  <span className="text-gray-800">John Trinh.</span>
+                </h1>
+              </div>
+              <div className="w-full z-10 text-gray-400 mt-4 md:mt-8 flex flex-col text-center md:text-left">
+                <h2>
+                  <span>Let's create elegant experiences,</span>
+                </h2>
+                <h2>
+                  <span> bounce ideas and deliver great work.</span>
+                </h2>
+              </div>
+            </div>
+
+            {/* On larger screens, image comes after hero text. */}
+            <div className="w-0 md:w-full h-0 md:h-screen flex items-center justify-center">
+              <div className="w-0 md:w-48 h-0 md:h-48">
+                <img src="image_1.png"></img>
+              </div>
+            </div>
           </div>
-        </div>
+          
+        </InView>
 
-        <div id="blog" className="relative w-full h-screen flex flex-col items-center justify-center">
-          <span className="z-10 tracking-tighter font-display font-bold text-6xl text-gray-800">Blog</span>
-        </div>
+        <InView className="w-full" threshold={0.5} onChange={(inView, entry) => setIsBlogVisible(inView)}>
+          <div id="blog" className="relative w-full h-screen flex flex-col items-center justify-center">
+            <span className="z-10 tracking-tighter font-display font-bold text-6xl text-gray-800">Blog</span>
+          </div>
+        </InView>
 
-        <div id="projects" className="relative w-full h-screen flex flex-col items-center justify-center">
-          <span className="z-10 tracking-tighter font-display font-bold text-6xl text-gray-800">Projects</span>
-        </div>
+        <InView className="w-full" threshold={0.5} onChange={(inView, entry) => setIsProjectsVisible(inView)}>
+          <div id="projects" className="relative w-full h-screen flex flex-col items-center justify-center">
+            <span className="z-10 tracking-tighter font-display font-bold text-6xl text-gray-800">Projects</span>
+          </div>
+        </InView>
 
-        <div id="contact" className="relative w-full h-screen flex flex-col items-center justify-center">
-          <span className="z-10 tracking-tighter font-display font-bold text-6xl text-gray-800">Contact</span>
-        </div>
-
+        <InView className="w-full" threshold={0.5} onChange={(inView, entry) => setIsContactVisible(inView)}>
+          <div id="contact" className="relative w-full h-screen flex flex-col items-center justify-center">
+            <span className="z-10 tracking-tighter font-display font-bold text-6xl text-gray-800">Contact</span>
+          </div>
+        </InView>
       </main>
 
 {/*
