72bb6358aac1417b2a122d88209af175323bbc77
John Trinh <john@johns-air.myfiosgateway.com>
2021-10-01 08:39:22 -0400

    fix for extra side space

--- components/Nav.js
+++ components/Nav.js
@@ -9,7 +9,7 @@ export default function Nav(props) {
     const contactUnderline = (props.isContactVisible) ? <div className="absolute bottom-0 -mb-2 rounded-md h-1 w-full bg-blue-300 transition-all duration-200 opacity-100"></div> : <div className="absolute bottom-0 -mb-2 rounded-md h-1 w-full transition-all duration-200 opacity-0"></div>
 
     return (
-        <nav className="fixed top-0 z-20 w-full flex px-8 self-end justify-end flex-row space-x-8 h-16 items-center text-gray-400 bg-white">
+        <nav className="fixed top-0 z-20 flex px-8 flex-row space-x-8 h-16 items-center text-gray-400 bg-white">
             <Link href="/#home">
                 <div className="relative flex flex-col">
                     <a className="cursor-pointer hover:text-blue-300">Home</a>
