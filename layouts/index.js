58533d36792754d3de6270d7636594e1dd1d9045
John Trinh <john@johns-air.myfiosgateway.com>
2021-09-30 21:44:37 -0400

    debugging vertical padding class being removed

--- layouts/index.js
+++ layouts/index.js
@@ -15,7 +15,7 @@ export default function BlogPage({ children, frontMatter }) {
     return (
       <div className={ styles.blogpage }>
         <Nav isHomeVisible={false} isBlogVisible={false} isProjectsVisible={false} isContactVisible={false} />
-        <section className="flex w-screen h-full flex-col px-4 md:px-36 lg:px-64 py-24">
+        <section className="flex w-screen h-full flex-col py-24 px-4 md:px-36 lg:px-64">
           <span className="flex flex-row bg-gray-100 p-4 rounded-md">
             <span className="w-3/4 text-gray-800 flex flex-row tracking-tight font-display space-x-2 items-end">
               <Link href="/blog"><span className="text-gray-400 font-normal tracking-normal cursor-pointer hover:text-gray-600">Posts /</span></Link>
